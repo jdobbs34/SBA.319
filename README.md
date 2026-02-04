43ec06c40e7d97cc9c55d4bf80fb8d02dcb4dfae
jdobbs34 <dobbsjason34@yahoo.com>
2026-02-04 14:39:56 -0700

    2nd commit

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# MongoDB Database Application
+# SBA 319 - MongoDB Database Application
+
+https://github.com/jdobbs34/SBA.319
+
+
---
+++ server.js
@@ -0,0 +1,17 @@
+// Imports
+import express from "express";
+
+// Setups
+const app = express();
+const PORT = process.env.PORT || 3001
+
+// Middleware
+
+// Route
+
+// Global Err
+
+// Listener
+app.listen(PORT, () => {
+  console.log(`Server Listening on PORT: ${PORT}`);
+});
