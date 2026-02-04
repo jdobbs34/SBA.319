368ee896565172e2897add477ddfc49e86cf2a60
jdobbs34 <dobbsjason34@yahoo.com>
2026-02-04 15:16:31 -0700

    4th commit

--- server.js
+++ server.js
@@ -1,15 +1,19 @@
 // Imports
 import express from "express";
+import { logReq, globalErr } from "./middleware/middlewares.js"
 
 // Setups
 const app = express();
 const PORT = process.env.PORT || 3001
 
 // Middleware
+app.use(express.json());
+app.use(logReq);
 
 // Route
 
 // Global Err
+app.use(globalErr)
 
 // Listener
 app.listen(PORT, () => {
