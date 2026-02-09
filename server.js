f207775ceab38b3377cdc599ee758f7c5a937062
jdobbs34 <dobbsjason34@yahoo.com>
2026-02-08 19:02:36 -0700

    2nd commit

---
+++ server.js
@@ -0,0 +1,16 @@
+// Imports
+import express from "express";
+import dotenv from "dotenv";
+
+// Setups
+dotenv.config();
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+// Middleware
+
+// Route
+
+// Global Err
+
+// Listener
