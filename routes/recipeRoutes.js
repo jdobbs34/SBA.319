c7f81624259c39000d66685401e6c70dfe392b43
jdobbs34 <dobbsjason34@yahoo.com>
2026-02-09 10:49:26 -0700

    10th commit

--- routes/recipeRoutes.js
+++ routes/recipeRoutes.js
@@ -18,3 +18,34 @@ router
     res.json(allRecipes);
   });
 
+// Update
+router
+  .route("/:id")
+  .put(async (req, res) => {
+    let updatedRecipe = await Recipes.findByIdAndUpdate(
+      req.params.id,
+      req.body,
+      { new: true }, 
+    );
+
+    res.json(updatedRecipe);
+  })
+   // Delete
+  .delete(async (req, res) => {
+    let deletedRecipe = await Recipes.findByIdAndDelete(req.params.id);
+
+    if (!deletedRecipe) return res.status(404).json({ error: "Recipe Not Found" });
+
+    res.json(deletedRecipe);
+  })
+
+  // Show One
+  .get(async (req, res) => {
+    let recipe = await Recipes.findById(req.params.id);
+
+    if (!recipe) return res.status(404).json({ error: "Recipe Not Found" });
+
+    res.json(recipe);
+  });
+
+export default router;
